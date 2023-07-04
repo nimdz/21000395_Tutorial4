f5e576050ee9ed7fb7a6ee5fa39cae48b6b70038
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-07-04 15:45:02 +0530

    Update Ex3.scala

--- src/main/scala/Ex3.scala
+++ src/main/scala/Ex3.scala
@@ -3,14 +3,17 @@ object Ex3 extends App {
 
   def toLower(s:String):String=s.toLowerCase()
 
-  def formatNames(name: String)(formatter: String => String): String = formatter(name)
+  def formatNames(name: String,formatter: String => String): String = formatter(name)
 
   var list1=Array("Benny", "Niroshan", "Saman","Kumara")
 
-  println(toUpper(list1(0)))
-  println(list1(1).charAt(0)+toUpper(list1(1).charAt(1).toString)+list1(1).substring(2,8))
-  println(toLower(list1(2)))
-  println(list1(3).substring(0,5)+toUpper(list1(3).charAt(5).toString))
+  println(formatNames(list1(0),toUpper))
+  println(list1(1).charAt(0)+formatNames(list1(1).charAt(1).toString,toUpper)+list1(1).substring(2,8))
+  println(formatNames(list1(2),toLower))
+  println(list1(3).substring(0,5)+formatNames(list1(3).charAt(5).toString,toUpper))
+
+
+
 
 
 }
